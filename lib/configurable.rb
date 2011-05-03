a6edfd03d752dcb74db6a9f7a56d296670f5daba
Kevin Bullock <kbullock@umn.edu>
2011-05-03 15:57:31 -0500

    spec Configurable

--- lib/configurable.rb
+++ lib/configurable.rb
@@ -1,4 +1,3 @@
-require 'configurable/core_ext'
 require 'configurable/config_struct'
 
 module Configurable
@@ -10,6 +9,13 @@ module Configurable
 
   module ClassMethods
     def configurable_options(opts = {})
+      @_config_struct = ConfigStruct::Struct.new(*opts.keys)
+    end
+
+    def config
+      raise NameError, 'use configurable_options to define settings' unless
+        @_config_struct
+      @_config ||= @_config_struct.new
     end
   end
 end
--- test/test_configurable.rb
+++ test/test_configurable.rb
@@ -3,9 +3,31 @@ require 'minitest/autorun'
 
 require 'configurable'
 
-class ConfigurableTester
-  include Configurable
-end
+describe Configurable do
+  before do
+    @test_class = Class.new
+    @test_class.instance_eval do
+      include Configurable
+    end
+  end
 
-class TestConfigurable < MiniTest::Unit::TestCase
+  describe 'before configurable_options is called' do
+    describe 'config' do
+      it 'should raise NameError' do
+        assert_raises(NameError) { @test_class.config }
+      end
+    end
+  end
+
+  describe 'configurable_options' do
+    before do
+      @test_class.instance_eval do
+        configurable_options :one => 1, :two => nil
+      end
+    end
+
+    it 'should create the config struct' do
+      assert @test_class.instance_eval { @_config_struct }.ancestors.include? ConfigStruct::Struct
+    end
+  end
 end
